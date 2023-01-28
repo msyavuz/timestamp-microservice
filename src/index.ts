4f1b68941416b661d0f98bdcedb8325145abbf11
msyavuz <salih.yavuz@metu.edu.tr>
2023-01-28 20:33:37 +0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,54 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.static("public"));
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "../public/index.html");
 });
 
-app.get("/api/:q", (req, res) => {
+app.get("/api/:q?", (req, res) => {
     const q = req.params.q;
-    try {
-        const numForm = parseInt(q);
-        if (!q.includes("-")) {
-            const date = new Date(numForm);
-            const unix = date.valueOf();
-            const utc = date.toUTCString();
+    if (q) {
+        try {
+            const numForm = parseInt(q);
+            if (!q.includes("-")) {
+                const date = new Date(numForm);
+                const unix = date.valueOf();
+                const utc = date.toUTCString();
 
-            res.send({
-                unix,
-                utc,
-            });
-        } else {
-            const date = new Date(q);
-            const unix = date.getTime() / 1000;
-            const utc = date.toUTCString();
-            res.send({
-                unix,
-                utc,
-            });
+                res.send({
+                    unix,
+                    utc,
+                });
+            } else {
+                const date = new Date(q);
+                const unix = date.getTime() / 1000;
+                const utc = date.toUTCString();
+                res.send({
+                    unix,
+                    utc,
+                });
+            }
+        } catch {
+            res.send({ error: "Invalid Date" });
         }
-    } catch {
-        res.sendStatus(404);
+    } else {
+        const date = new Date();
+        const unix = date.getTime();
+        const utc = date.toUTCString();
+        res.send({
+            unix,
+            utc,
+        });
     }
 });
 
